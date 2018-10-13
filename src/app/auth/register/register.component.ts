f21f130bdc78cddbae80eb672f438f3adbeaaa0d
Michael Treser <armikael2@gmail.com>
2018-10-13 17:12:27 +0300

    Created Register page.

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { FormGroup, FormControl, Validators, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public userForm: any;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  userForm = new FormGroup({
+    fullname: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  get fullname(): any { return this.userForm.get('fullname'); }
+  get email(): any { return this.userForm.get('email'); }
+  get password(): any { return this.userForm.get('password'); }
+
+  register() {
+    if (this.userForm.invalid()) { return; }
+
+    this.authService.register();
   }
 
 }
