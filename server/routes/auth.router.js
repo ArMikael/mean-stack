b4006a07ac85ff6240e7ae9def81e4ee5758c100
Michael Treser <armikael2@gmail.com>
2018-10-15 16:55:19 +0300

    Server. Added POST user validation.

--- server/routes/auth.router.js
+++ server/routes/auth.router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Joi = require('joi');
 
 router.get('/', async (req, res) => {
   res.status(200).send('Welcome to the AUTH');
@@ -7,8 +8,29 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  res.status(200).send('POST OK.');
-  console.log('Auth POST');
+  try {
+    let { error } = await validateUser(req.body);
+    if (error) { return res.status(400).send('Wrong user parameters: ', error.details[0].message); }
+
+    res.status(200).send('POST OK.');
+    console.log('Auth POST');
+  }
+
+  catch(ex) {
+    console.log(ex.message);
+    res.send(ex.message);
+  }
+
+  function validateUser(newUser) {
+    const userSchema = {
+      fullName: Joi.string().min(3).required(),
+      email: Joi.string().email().required,
+      password: Joi.string.min(5).required()
+    };
+
+    return Joi.validate(newUser, userSchema);
+  }
+
 });
 
 module.exports = router;
