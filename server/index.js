5fc5093c7859b4ac88e5bf16b321e4d78b07b56c
Michael Treser <armikael2@gmail.com>
2018-10-18 13:46:43 +0300

    MongoDB created and connected to the app.

--- server/index.js
+++ server/index.js
@@ -2,10 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const auth = require('./routes/auth.router');
+const users = require('./routes/user.router');
+const { privateConfig } = require('./config/private-config');
+
 const app = express();
 
 app.use(express.json());
 app.use('/api/auth', auth);
+app.use('/api/users', users);
+
+mongoose.connect(privateConfig.mdbMeanStackConnectionString, { useNewUrlParser: true })
+  .then(()=> console.log('Connected to MongoDB.'))
+  .catch(() => console.log('Could not connect to MongoDB'));
 
 app.get('/', (req, res) => {
   res.status(200).send('Welcome to the Store')
