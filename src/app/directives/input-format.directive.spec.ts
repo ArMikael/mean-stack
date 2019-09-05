fb516ade834b026872ab57dc7aef3687f59d4ee0
Michael Treser <armikael2@gmail.com>
2019-09-05 18:22:11 +0300

    package.json updated to fix tests.

--- src/app/directives/input-format.directive.spec.ts
+++ src/app/directives/input-format.directive.spec.ts
@@ -1,8 +1,13 @@
 import { InputFormatDirective } from './input-format.directive';
+import { describe, expect } from '@angular/core/testing/src/testing_internal';
+
+import {ElementRef} from '@angular/core';
+
 
 describe('InputFormatDirective', () => {
+
   it('should create an instance', () => {
-    const directive = new InputFormatDirective();
+    const directive = new InputFormatDirective(<any>ElementRef);
     expect(directive).toBeTruthy();
   });
 });
